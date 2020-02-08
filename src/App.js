b43f86ec7fb9c849515cbcf0540b6b81ee4eadc3
Matheus Goncalves <matheusgoncalves@MacBook-Air-de-Matheus.local>
2020-02-08 15:48:05 -0300

    add facebook layout

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './App.css';
+import profile from './assets/eu.JPG';
+import Header from './components/Header';
+import PostList from './components/PostList';
+
+function App() {
+  return (
+    <div>
+    <Header />
+    <PostList />
+    </div>
+
+  )
+}
+
+export default App;
